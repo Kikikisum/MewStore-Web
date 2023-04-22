3d3794854bed58943a7f7d1da8ad8e1aec38f3a4
Kikikisum <2313810210@qq.com>
2023-04-22 16:43:34 +0800

    管理员审核商品+审核举报信息+审核冻结状态的用户

--- Verify.py
+++ Verify.py
@@ -41,35 +41,6 @@ def verify_report():
         print(e)
 
 
-# 卖家发出找回账号请求
-@app.route('/sell/return/account', methods=['POST'])
-def return_account():
-    try:
-        id = request.form.get('id')     # 用户id
-        order_id = request.form.get('order_id')  # 对订单发起的账号请求的订单id
-        user = session.query(User).filter(User.id == id).first()
-        order = session.query(Order).filter(Order.id == order_id).first()
-        print(order)
-        good_id = order.good_id
-        status = user.status    # 卖家状态
-        good_status = session.query(Good).filter(Good.id == good_id).first().status
-        print(user)
-        if good_status == 3:
-            if status == 1:
-                return jsonify(dict(code=201, message='黑户没有权限申请'))
-            else:
-                if status == 0 or status == 3:
-                    session.query(User).filter(User.id == id).update({'status': 2})
-                    session.commit()
-                    return jsonify(dict(code=201, message='账号找回申请成功'))
-                else:
-                    return jsonify(dict(code=201, message='被冻结状态没有权限申请'))
-        else:
-            return jsonify(dict(code=201, message='订单还未完成，不可找回'))
-    except Exception as e:
-        print(e)
-
-
 @app.route('/manage/verify/money/', methods=['POST'])
 def verify_money():
     try:
